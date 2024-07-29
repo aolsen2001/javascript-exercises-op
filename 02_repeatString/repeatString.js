7b9291912b1fc42a41cff0041de88851beb20d3f
aolsen2001 <174476920+aolsen2001@users.noreply.github.com>
2024-07-28 20:10:36 -0500

    Complete exercises 1 through 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(s, n) {
+    if(n < 0) return "ERROR";
+    let repeat = "";
+    for (let i = 0; i < n; ++i) {
+        repeat += s;
+    }
+    return repeat;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    let rev = "";
+    for (let i = s.length; i >= 0; --i) {
+        rev += s.charAt(i);
+    }
+    return rev;
 };
 
 // Do not edit below this line
